3e02bf1ae1cd2b16996d5ed241cafcd5e16a0db7
Lokesh-technical <lokesh.patil.5057@gmail.com>
2024-06-21 16:23:25 +0530

    Update a pakage.json file

--- json-server/server.js
+++ json-server/server.js
@@ -1,15 +1,23 @@
-// json-server/server.js
 const jsonServer = require('json-server');
 const path = require('path');
-
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 8000;
+
+// Use default middlewares (logger, static, cors, and no-cache)
 server.use(middlewares);
+
+// Custom route middleware for more logging
+server.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+});
+
+// Use default router
 server.use(router);
 
-const port = process.env.PORT || 8000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
